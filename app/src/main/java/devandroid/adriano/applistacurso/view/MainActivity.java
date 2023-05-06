6d1ec21dee3602b9d16ae20fa5e4988662e9321d
= <adriano.cvasconcelos@hotmail.com>
2023-05-06 11:38:01 -0300

    App Lista de interessados Curso Android, Arquitetura MVC Creat Pessoa

--- app/src/main/java/devandroid/adriano/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/adriano/applistacurso/view/MainActivity.java
@@ -1,17 +1,25 @@
 package devandroid.adriano.applistacurso.view;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import devandroid.adriano.applistacurso.R;
+import devandroid.adriano.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Adriano");
+        pessoa.setSobreNome("Vasconcelos");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("85 9999 1234");
+
     }
 }
